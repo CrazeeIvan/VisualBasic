a565c40aad4256b9d744ff009035f32fcd6ea05e
unknown <blue20@RM02-20.fasfg.local>
2015-09-02 15:33:56 +0100

    v0.2 - Initial GUI & code structure

--- projTrainees/projTrainees/My Project/Application.Designer.vb
+++ projTrainees/projTrainees/My Project/Application.Designer.vb
@@ -32,7 +32,7 @@ Namespace My
         
         <Global.System.Diagnostics.DebuggerStepThroughAttribute()>  _
         Protected Overrides Sub OnCreateMainForm()
-            Me.MainForm = Global.projTrainees.Form1
+            Me.MainForm = Global.projTrainees.frmMain
         End Sub
     End Class
 End Namespace
---
+++ projTrainees/projTrainees/frmMain.Designer.vb
@@ -0,0 +1,470 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class frmMain
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.txtID = New System.Windows.Forms.TextBox()
+        Me.txtFirstName = New System.Windows.Forms.TextBox()
+        Me.txtLastName = New System.Windows.Forms.TextBox()
+        Me.txtAddress1 = New System.Windows.Forms.TextBox()
+        Me.txtAddress2 = New System.Windows.Forms.TextBox()
+        Me.txtCountry = New System.Windows.Forms.TextBox()
+        Me.txtCounty = New System.Windows.Forms.TextBox()
+        Me.txtPhone = New System.Windows.Forms.TextBox()
+        Me.txtEmail = New System.Windows.Forms.TextBox()
+        Me.dtDoB = New System.Windows.Forms.DateTimePicker()
+        Me.chkMale = New System.Windows.Forms.CheckBox()
+        Me.chkFemale = New System.Windows.Forms.CheckBox()
+        Me.txtNotes = New System.Windows.Forms.RichTextBox()
+        Me.lblID = New System.Windows.Forms.Label()
+        Me.lblFirstName = New System.Windows.Forms.Label()
+        Me.lblLastName = New System.Windows.Forms.Label()
+        Me.lblAddress1 = New System.Windows.Forms.Label()
+        Me.lblAddress2 = New System.Windows.Forms.Label()
+        Me.lblCountry = New System.Windows.Forms.Label()
+        Me.lblCounty = New System.Windows.Forms.Label()
+        Me.lblPhone = New System.Windows.Forms.Label()
+        Me.lblEmail = New System.Windows.Forms.Label()
+        Me.lblDob = New System.Windows.Forms.Label()
+        Me.lblGender = New System.Windows.Forms.Label()
+        Me.lblNotes = New System.Windows.Forms.Label()
+        Me.txtDoB = New System.Windows.Forms.TextBox()
+        Me.txtGender = New System.Windows.Forms.TextBox()
+        Me.btnFirst = New System.Windows.Forms.Button()
+        Me.btnPrevious = New System.Windows.Forms.Button()
+        Me.txtIndex = New System.Windows.Forms.TextBox()
+        Me.btnNext = New System.Windows.Forms.Button()
+        Me.btnLast = New System.Windows.Forms.Button()
+        Me.btnExit = New System.Windows.Forms.Button()
+        Me.btnAdd = New System.Windows.Forms.Button()
+        Me.btnEdit = New System.Windows.Forms.Button()
+        Me.btnDelete = New System.Windows.Forms.Button()
+        Me.btnSave = New System.Windows.Forms.Button()
+        Me.btnCancel = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'txtID
+        '
+        Me.txtID.Location = New System.Drawing.Point(127, 25)
+        Me.txtID.Name = "txtID"
+        Me.txtID.Size = New System.Drawing.Size(100, 20)
+        Me.txtID.TabIndex = 0
+        '
+        'txtFirstName
+        '
+        Me.txtFirstName.Location = New System.Drawing.Point(127, 51)
+        Me.txtFirstName.Name = "txtFirstName"
+        Me.txtFirstName.Size = New System.Drawing.Size(100, 20)
+        Me.txtFirstName.TabIndex = 1
+        '
+        'txtLastName
+        '
+        Me.txtLastName.Location = New System.Drawing.Point(127, 77)
+        Me.txtLastName.Name = "txtLastName"
+        Me.txtLastName.Size = New System.Drawing.Size(100, 20)
+        Me.txtLastName.TabIndex = 2
+        '
+        'txtAddress1
+        '
+        Me.txtAddress1.Location = New System.Drawing.Point(127, 104)
+        Me.txtAddress1.Name = "txtAddress1"
+        Me.txtAddress1.Size = New System.Drawing.Size(100, 20)
+        Me.txtAddress1.TabIndex = 3
+        '
+        'txtAddress2
+        '
+        Me.txtAddress2.Location = New System.Drawing.Point(127, 131)
+        Me.txtAddress2.Name = "txtAddress2"
+        Me.txtAddress2.Size = New System.Drawing.Size(100, 20)
+        Me.txtAddress2.TabIndex = 4
+        '
+        'txtCountry
+        '
+        Me.txtCountry.Location = New System.Drawing.Point(127, 158)
+        Me.txtCountry.Name = "txtCountry"
+        Me.txtCountry.Size = New System.Drawing.Size(100, 20)
+        Me.txtCountry.TabIndex = 5
+        '
+        'txtCounty
+        '
+        Me.txtCounty.Location = New System.Drawing.Point(127, 185)
+        Me.txtCounty.Name = "txtCounty"
+        Me.txtCounty.Size = New System.Drawing.Size(100, 20)
+        Me.txtCounty.TabIndex = 6
+        '
+        'txtPhone
+        '
+        Me.txtPhone.Location = New System.Drawing.Point(127, 212)
+        Me.txtPhone.Name = "txtPhone"
+        Me.txtPhone.Size = New System.Drawing.Size(100, 20)
+        Me.txtPhone.TabIndex = 7
+        '
+        'txtEmail
+        '
+        Me.txtEmail.Location = New System.Drawing.Point(127, 239)
+        Me.txtEmail.Name = "txtEmail"
+        Me.txtEmail.Size = New System.Drawing.Size(100, 20)
+        Me.txtEmail.TabIndex = 8
+        '
+        'dtDoB
+        '
+        Me.dtDoB.Location = New System.Drawing.Point(127, 266)
+        Me.dtDoB.Name = "dtDoB"
+        Me.dtDoB.Size = New System.Drawing.Size(200, 20)
+        Me.dtDoB.TabIndex = 9
+        '
+        'chkMale
+        '
+        Me.chkMale.AutoSize = True
+        Me.chkMale.Location = New System.Drawing.Point(127, 293)
+        Me.chkMale.Name = "chkMale"
+        Me.chkMale.Size = New System.Drawing.Size(49, 17)
+        Me.chkMale.TabIndex = 10
+        Me.chkMale.Text = "Male"
+        Me.chkMale.UseVisualStyleBackColor = True
+        '
+        'chkFemale
+        '
+        Me.chkFemale.AutoSize = True
+        Me.chkFemale.Location = New System.Drawing.Point(215, 293)
+        Me.chkFemale.Name = "chkFemale"
+        Me.chkFemale.Size = New System.Drawing.Size(60, 17)
+        Me.chkFemale.TabIndex = 11
+        Me.chkFemale.Text = "Female"
+        Me.chkFemale.UseVisualStyleBackColor = True
+        '
+        'txtNotes
+        '
+        Me.txtNotes.Location = New System.Drawing.Point(127, 343)
+        Me.txtNotes.Name = "txtNotes"
+        Me.txtNotes.Size = New System.Drawing.Size(485, 146)
+        Me.txtNotes.TabIndex = 13
+        Me.txtNotes.Text = ""
+        '
+        'lblID
+        '
+        Me.lblID.AutoSize = True
+        Me.lblID.Location = New System.Drawing.Point(12, 25)
+        Me.lblID.Name = "lblID"
+        Me.lblID.Size = New System.Drawing.Size(18, 13)
+        Me.lblID.TabIndex = 14
+        Me.lblID.Text = "ID"
+        '
+        'lblFirstName
+        '
+        Me.lblFirstName.AutoSize = True
+        Me.lblFirstName.Location = New System.Drawing.Point(12, 51)
+        Me.lblFirstName.Name = "lblFirstName"
+        Me.lblFirstName.Size = New System.Drawing.Size(57, 13)
+        Me.lblFirstName.TabIndex = 15
+        Me.lblFirstName.Text = "First Name"
+        '
+        'lblLastName
+        '
+        Me.lblLastName.AutoSize = True
+        Me.lblLastName.Location = New System.Drawing.Point(13, 77)
+        Me.lblLastName.Name = "lblLastName"
+        Me.lblLastName.Size = New System.Drawing.Size(58, 13)
+        Me.lblLastName.TabIndex = 16
+        Me.lblLastName.Text = "Last Name"
+        '
+        'lblAddress1
+        '
+        Me.lblAddress1.AutoSize = True
+        Me.lblAddress1.Location = New System.Drawing.Point(12, 104)
+        Me.lblAddress1.Name = "lblAddress1"
+        Me.lblAddress1.Size = New System.Drawing.Size(54, 13)
+        Me.lblAddress1.TabIndex = 17
+        Me.lblAddress1.Text = "Address 1"
+        '
+        'lblAddress2
+        '
+        Me.lblAddress2.AutoSize = True
+        Me.lblAddress2.Location = New System.Drawing.Point(12, 131)
+        Me.lblAddress2.Name = "lblAddress2"
+        Me.lblAddress2.Size = New System.Drawing.Size(54, 13)
+        Me.lblAddress2.TabIndex = 18
+        Me.lblAddress2.Text = "Address 2"
+        '
+        'lblCountry
+        '
+        Me.lblCountry.AutoSize = True
+        Me.lblCountry.Location = New System.Drawing.Point(12, 158)
+        Me.lblCountry.Name = "lblCountry"
+        Me.lblCountry.Size = New System.Drawing.Size(43, 13)
+        Me.lblCountry.TabIndex = 19
+        Me.lblCountry.Text = "Country"
+        '
+        'lblCounty
+        '
+        Me.lblCounty.AutoSize = True
+        Me.lblCounty.Location = New System.Drawing.Point(12, 185)
+        Me.lblCounty.Name = "lblCounty"
+        Me.lblCounty.Size = New System.Drawing.Size(40, 13)
+        Me.lblCounty.TabIndex = 20
+        Me.lblCounty.Text = "County"
+        '
+        'lblPhone
+        '
+        Me.lblPhone.AutoSize = True
+        Me.lblPhone.Location = New System.Drawing.Point(12, 212)
+        Me.lblPhone.Name = "lblPhone"
+        Me.lblPhone.Size = New System.Drawing.Size(38, 13)
+        Me.lblPhone.TabIndex = 21
+        Me.lblPhone.Text = "Phone"
+        '
+        'lblEmail
+        '
+        Me.lblEmail.AutoSize = True
+        Me.lblEmail.Location = New System.Drawing.Point(12, 239)
+        Me.lblEmail.Name = "lblEmail"
+        Me.lblEmail.Size = New System.Drawing.Size(32, 13)
+        Me.lblEmail.TabIndex = 22
+        Me.lblEmail.Text = "Email"
+        '
+        'lblDob
+        '
+        Me.lblDob.AutoSize = True
+        Me.lblDob.Location = New System.Drawing.Point(13, 266)
+        Me.lblDob.Name = "lblDob"
+        Me.lblDob.Size = New System.Drawing.Size(66, 13)
+        Me.lblDob.TabIndex = 23
+        Me.lblDob.Text = "Date of Birth"
+        '
+        'lblGender
+        '
+        Me.lblGender.AutoSize = True
+        Me.lblGender.Location = New System.Drawing.Point(16, 293)
+        Me.lblGender.Name = "lblGender"
+        Me.lblGender.Size = New System.Drawing.Size(42, 13)
+        Me.lblGender.TabIndex = 24
+        Me.lblGender.Text = "Gender"
+        '
+        'lblNotes
+        '
+        Me.lblNotes.AutoSize = True
+        Me.lblNotes.Location = New System.Drawing.Point(15, 343)
+        Me.lblNotes.Name = "lblNotes"
+        Me.lblNotes.Size = New System.Drawing.Size(35, 13)
+        Me.lblNotes.TabIndex = 25
+        Me.lblNotes.Text = "Notes"
+        '
+        'txtDoB
+        '
+        Me.txtDoB.Location = New System.Drawing.Point(127, 267)
+        Me.txtDoB.Name = "txtDoB"
+        Me.txtDoB.Size = New System.Drawing.Size(200, 20)
+        Me.txtDoB.TabIndex = 26
+        '
+        'txtGender
+        '
+        Me.txtGender.Location = New System.Drawing.Point(127, 293)
+        Me.txtGender.Name = "txtGender"
+        Me.txtGender.Size = New System.Drawing.Size(100, 20)
+        Me.txtGender.TabIndex = 27
+        '
+        'btnFirst
+        '
+        Me.btnFirst.Location = New System.Drawing.Point(152, 525)
+        Me.btnFirst.Name = "btnFirst"
+        Me.btnFirst.Size = New System.Drawing.Size(75, 23)
+        Me.btnFirst.TabIndex = 28
+        Me.btnFirst.Text = "<< First"
+        Me.btnFirst.UseVisualStyleBackColor = True
+        '
+        'btnPrevious
+        '
+        Me.btnPrevious.Location = New System.Drawing.Point(240, 525)
+        Me.btnPrevious.Name = "btnPrevious"
+        Me.btnPrevious.Size = New System.Drawing.Size(75, 23)
+        Me.btnPrevious.TabIndex = 29
+        Me.btnPrevious.Text = "< Previous"
+        Me.btnPrevious.UseVisualStyleBackColor = True
+        '
+        'txtIndex
+        '
+        Me.txtIndex.Location = New System.Drawing.Point(320, 527)
+        Me.txtIndex.Name = "txtIndex"
+        Me.txtIndex.Size = New System.Drawing.Size(100, 20)
+        Me.txtIndex.TabIndex = 30
+        '
+        'btnNext
+        '
+        Me.btnNext.Location = New System.Drawing.Point(427, 525)
+        Me.btnNext.Name = "btnNext"
+        Me.btnNext.Size = New System.Drawing.Size(75, 23)
+        Me.btnNext.TabIndex = 31
+        Me.btnNext.Text = "Next >"
+        Me.btnNext.UseVisualStyleBackColor = True
+        '
+        'btnLast
+        '
+        Me.btnLast.Location = New System.Drawing.Point(515, 525)
+        Me.btnLast.Name = "btnLast"
+        Me.btnLast.Size = New System.Drawing.Size(75, 23)
+        Me.btnLast.TabIndex = 32
+        Me.btnLast.Text = "Last >>"
+        Me.btnLast.UseVisualStyleBackColor = True
+        '
+        'btnExit
+        '
+        Me.btnExit.Location = New System.Drawing.Point(908, 543)
+        Me.btnExit.Name = "btnExit"
+        Me.btnExit.Size = New System.Drawing.Size(75, 23)
+        Me.btnExit.TabIndex = 33
+        Me.btnExit.Text = "Exit"
+        Me.btnExit.UseVisualStyleBackColor = True
+        '
+        'btnAdd
+        '
+        Me.btnAdd.Location = New System.Drawing.Point(908, 125)
+        Me.btnAdd.Name = "btnAdd"
+        Me.btnAdd.Size = New System.Drawing.Size(75, 23)
+        Me.btnAdd.TabIndex = 34
+        Me.btnAdd.Text = "Add"
+        Me.btnAdd.UseVisualStyleBackColor = True
+        '
+        'btnEdit
+        '
+        Me.btnEdit.Location = New System.Drawing.Point(908, 154)
+        Me.btnEdit.Name = "btnEdit"
+        Me.btnEdit.Size = New System.Drawing.Size(75, 23)
+        Me.btnEdit.TabIndex = 35
+        Me.btnEdit.Text = "Edit"
+        Me.btnEdit.UseVisualStyleBackColor = True
+        '
+        'btnDelete
+        '
+        Me.btnDelete.Location = New System.Drawing.Point(908, 183)
+        Me.btnDelete.Name = "btnDelete"
+        Me.btnDelete.Size = New System.Drawing.Size(75, 23)
+        Me.btnDelete.TabIndex = 36
+        Me.btnDelete.Text = "Delete"
+        Me.btnDelete.UseVisualStyleBackColor = True
+        '
+        'btnSave
+        '
+        Me.btnSave.Location = New System.Drawing.Point(908, 212)
+        Me.btnSave.Name = "btnSave"
+        Me.btnSave.Size = New System.Drawing.Size(75, 23)
+        Me.btnSave.TabIndex = 37
+        Me.btnSave.Text = "Save"
+        Me.btnSave.UseVisualStyleBackColor = True
+        '
+        'btnCancel
+        '
+        Me.btnCancel.Location = New System.Drawing.Point(908, 241)
+        Me.btnCancel.Name = "btnCancel"
+        Me.btnCancel.Size = New System.Drawing.Size(75, 23)
+        Me.btnCancel.TabIndex = 38
+        Me.btnCancel.Text = "Cancel"
+        Me.btnCancel.UseVisualStyleBackColor = True
+        '
+        'frmMain
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(1017, 629)
+        Me.Controls.Add(Me.btnCancel)
+        Me.Controls.Add(Me.btnSave)
+        Me.Controls.Add(Me.btnDelete)
+        Me.Controls.Add(Me.btnEdit)
+        Me.Controls.Add(Me.btnAdd)
+        Me.Controls.Add(Me.btnExit)
+        Me.Controls.Add(Me.btnLast)
+        Me.Controls.Add(Me.btnNext)
+        Me.Controls.Add(Me.txtIndex)
+        Me.Controls.Add(Me.btnPrevious)
+        Me.Controls.Add(Me.btnFirst)
+        Me.Controls.Add(Me.txtGender)
+        Me.Controls.Add(Me.txtDoB)
+        Me.Controls.Add(Me.lblNotes)
+        Me.Controls.Add(Me.lblGender)
+        Me.Controls.Add(Me.lblDob)
+        Me.Controls.Add(Me.lblEmail)
+        Me.Controls.Add(Me.lblPhone)
+        Me.Controls.Add(Me.lblCounty)
+        Me.Controls.Add(Me.lblCountry)
+        Me.Controls.Add(Me.lblAddress2)
+        Me.Controls.Add(Me.lblAddress1)
+        Me.Controls.Add(Me.lblLastName)
+        Me.Controls.Add(Me.lblFirstName)
+        Me.Controls.Add(Me.lblID)
+        Me.Controls.Add(Me.txtNotes)
+        Me.Controls.Add(Me.chkFemale)
+        Me.Controls.Add(Me.chkMale)
+        Me.Controls.Add(Me.dtDoB)
+        Me.Controls.Add(Me.txtEmail)
+        Me.Controls.Add(Me.txtPhone)
+        Me.Controls.Add(Me.txtCounty)
+        Me.Controls.Add(Me.txtCountry)
+        Me.Controls.Add(Me.txtAddress2)
+        Me.Controls.Add(Me.txtAddress1)
+        Me.Controls.Add(Me.txtLastName)
+        Me.Controls.Add(Me.txtFirstName)
+        Me.Controls.Add(Me.txtID)
+        Me.Name = "frmMain"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents txtID As System.Windows.Forms.TextBox
+    Friend WithEvents txtFirstName As System.Windows.Forms.TextBox
+    Friend WithEvents txtLastName As System.Windows.Forms.TextBox
+    Friend WithEvents txtAddress1 As System.Windows.Forms.TextBox
+    Friend WithEvents txtAddress2 As System.Windows.Forms.TextBox
+    Friend WithEvents txtCountry As System.Windows.Forms.TextBox
+    Friend WithEvents txtCounty As System.Windows.Forms.TextBox
+    Friend WithEvents txtPhone As System.Windows.Forms.TextBox
+    Friend WithEvents txtEmail As System.Windows.Forms.TextBox
+    Friend WithEvents dtDoB As System.Windows.Forms.DateTimePicker
+    Friend WithEvents chkMale As System.Windows.Forms.CheckBox
+    Friend WithEvents chkFemale As System.Windows.Forms.CheckBox
+    Friend WithEvents txtNotes As System.Windows.Forms.RichTextBox
+    Friend WithEvents lblID As System.Windows.Forms.Label
+    Friend WithEvents lblFirstName As System.Windows.Forms.Label
+    Friend WithEvents lblLastName As System.Windows.Forms.Label
+    Friend WithEvents lblAddress1 As System.Windows.Forms.Label
+    Friend WithEvents lblAddress2 As System.Windows.Forms.Label
+    Friend WithEvents lblCountry As System.Windows.Forms.Label
+    Friend WithEvents lblCounty As System.Windows.Forms.Label
+    Friend WithEvents lblPhone As System.Windows.Forms.Label
+    Friend WithEvents lblEmail As System.Windows.Forms.Label
+    Friend WithEvents lblDob As System.Windows.Forms.Label
+    Friend WithEvents lblGender As System.Windows.Forms.Label
+    Friend WithEvents lblNotes As System.Windows.Forms.Label
+    Friend WithEvents txtDoB As System.Windows.Forms.TextBox
+    Friend WithEvents txtGender As System.Windows.Forms.TextBox
+    Friend WithEvents btnFirst As System.Windows.Forms.Button
+    Friend WithEvents btnPrevious As System.Windows.Forms.Button
+    Friend WithEvents txtIndex As System.Windows.Forms.TextBox
+    Friend WithEvents btnNext As System.Windows.Forms.Button
+    Friend WithEvents btnLast As System.Windows.Forms.Button
+    Friend WithEvents btnExit As System.Windows.Forms.Button
+    Friend WithEvents btnAdd As System.Windows.Forms.Button
+    Friend WithEvents btnEdit As System.Windows.Forms.Button
+    Friend WithEvents btnDelete As System.Windows.Forms.Button
+    Friend WithEvents btnSave As System.Windows.Forms.Button
+    Friend WithEvents btnCancel As System.Windows.Forms.Button
+
+End Class
